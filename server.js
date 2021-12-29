72ff8eae37dfb8b048fdd9117fc6025c9307436e
BAylwardWMAD <64170443+BAylwardWMAD@users.noreply.github.com>
2021-12-29 15:32:43 -0400

    Update

--- server.js
+++ server.js
@@ -1,22 +1,40 @@
-function requireHTTPS(req, res, next) {
-    // The 'x-forwarded-proto' check is for Heroku
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-        return res.redirect('https://' + req.get('host') + req.url);
-    }
-    next();
-}
+// function requireHTTPS(req, res, next) {
+//     // The 'x-forwarded-proto' check is for Heroku
+//     if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+//         return res.redirect('https://' + req.get('host') + req.url);
+//     }
+//     next();
+// }
+// const express = require('express');
+// const app = express();
+// const path = require('path');
+// const PORT = process.env.PORT || 8080;
+// // const PORT = process.env.PORT || 1337;
+
+// app.use(requireHTTPS);
+
+// app.use(express.static(path.resolve(__dirname + '/dist/winter2022/')));
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.resolve(__dirname + '/dist/winter2022/index.html'));
+// });
+
+// app.listen(PORT, () => {});
+
+
+//Install express server
 const express = require('express');
-const app = express();
 const path = require('path');
-const PORT = process.env.PORT || 8080;
-// const PORT = process.env.PORT || 1337;
 
-app.use(requireHTTPS);
+const app = express();
 
-app.use(express.static(path.resolve(__dirname + '/dist/winter2022/')));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/winter2022'));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname + '/dist/winter2022/index.html'));
+app.get('/*', function(req, res) {
+
+    res.sendFile(path.join(__dirname + '/dist/winter2022/index.html'));
 });
 
-app.listen(PORT, () => {});
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
