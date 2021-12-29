1f85e712ae74b77daba5607776b4aba1118f6f55
BAylwardWMAD <64170443+BAylwardWMAD@users.noreply.github.com>
2021-12-29 14:55:20 -0400

    Update

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-// app.use(requireHTTPS);
+app.use(requireHTTPS);
 
 app.use(express.static(`${__dirname}/dist/winter2022/`));
 
